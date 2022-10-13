ff8cc20c5cae7bdef29ba7e1bf76fa80dc03c3a5
Christian Lopes <chrtannus@gmail.com>
2022-10-13 10:57:15 -0300

    - The gene entry no longer has the collapsible description (linkouts) and the gene name links to the NCBI Gene page. - Adds toggle buttons that sort the list by gene NAME and RANK. -- #16

--- src/client/components/network-editor/gene-list-panel.js
+++ src/client/components/network-editor/gene-list-panel.js
@@ -1,21 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 import { NetworkEditorController } from './controller';
+import { EventEmitterProxy } from '../../../model/event-emitter-proxy';
 import theme from '../../theme';
 
 import { makeStyles } from '@material-ui/core/styles';
 
 import { List, ListSubheader, ListItem, ListItemText } from '@material-ui/core';
-import { Grid, Typography, Link, Divider } from '@material-ui/core';
+import { Grid, Typography, Link, Divider, Tooltip } from '@material-ui/core';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 import Skeleton from '@material-ui/lab/Skeleton';
 import HSBar from "react-horizontal-stacked-bar-chart";
 
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import SortByAlphaIcon from '@material-ui/icons/SortByAlpha';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 
-const CHART_WIDTH = 180;
+const CHART_WIDTH = 160;
 const CHART_HEIGHT = 14;
 
 const RANK_RANGE_COLOR = theme.palette.background.focus;
@@ -23,7 +27,13 @@ const UP_RANK_COLOR = '#f1a340';
 const DOWN_RANK_COLOR = '#998ec3';
 // (rank colors from: https://colorbrewer2.org/#type=diverging&scheme=PuOr&n=3)
 
-const linkoutProps = { target: "_blank",  rel: "noreferrer", color: "textSecondary", underline: "always" };
+const linkoutProps = { target: "_blank",  rel: "noreferrer", color: "textSecondary", underline: "hover" };
+
+const orderBy = {
+  alpha: { iteratees: ['gene'],       orders: ['asc'] },
+  up:    { iteratees: ['rank', 'gene'], orders: ['asc', 'asc'] },
+  down:  { iteratees: ['rank', 'gene'], orders: ['desc', 'asc'] },
+};
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -50,11 +60,12 @@ const useStyles = makeStyles((theme) => ({
     padding: '0 8px',
   },
   listItemtemIcon: {
-    cursor: 'pointer',
+    fontSize: '16px',
+    color: theme.palette.divider,
   },
   geneName: {
+    marginLeft: '0.5em',
     '&:hover': {
-      textDecoration: 'none',
       color: theme.palette.primary.light,
     }
   },
@@ -73,19 +84,27 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function GeneListPanel({ controller, searchResult }) {
+  const cy = controller.cy;
+
   const [clusterName, setClusterName] = useState(null);
   const [geneSetNames, setGeneSetNames] = useState([]);
   const [genes, setGenes] = useState([]);
-  const [selectedGene, setSelectedGene] = useState(0);
-  const [geneDescription, setGeneDescription] = useState(0);
-  
+  const [sort, setSort] = useState('down');
+
+  const sortRef = useRef(sort);
+  sortRef.current = sort;
+
   const classes = useStyles();
+
+  const sortGenes = (genes, sort) => {
+    const args = orderBy[sort];
+    return _.orderBy(genes, args.iteratees, args.orders);
+  };
 
   const fetchGeneList = async (geneSetNames) => {
     const res = await controller.fetchGeneList(geneSetNames);
     const genes = res ? res.genes : [];
-
-    setGenes(genes);
+    setGenes(sortGenes(genes, sortRef.current));
   };
 
   const fetchAllRankedGenes = async () => {
@@ -125,7 +144,7 @@ export function GeneListPanel({ controller, searchResult }) {
     setGenes([]);
 
     if (controller.isGeneListIndexed()) {
-      const eles = controller.cy.$(':selected');
+      const eles = cy.$(':selected');
 
       if (eles.length > 0) {
         fetchGeneListFromNodeOrEdge(eles[eles.length - 1]);
@@ -144,21 +163,24 @@ export function GeneListPanel({ controller, searchResult }) {
   };
 
   useEffect(() => {
+    const cyEmitter = new EventEmitterProxy(cy);
+
     controller.bus.on('geneListIndexed', onGeneListIndexed);
-    controller.cy.on('select unselect', onCySelectionChanged);
+    cyEmitter.on('select unselect', onCySelectionChanged);
 
     if (controller.isGeneListIndexed()) {
       if (searchResult) {
         setClusterName(null);
         setGeneSetNames([]);
-        setGenes(searchResult);
+        setGenes(sortGenes(searchResult, 'alpha'));
+        setSort('alpha');
       } else {
         debouncedSelectionHandler();
       }
     }
 
     return function cleanup() {
-      controller.cy.removeListener('select unselect', onCySelectionChanged);
+      cyEmitter.removeAllListeners();
       controller.bus.removeListener('geneListIndexed', onGeneListIndexed);
     };
   }, []);
@@ -211,57 +233,21 @@ export function GeneListPanel({ controller, searchResult }) {
       }
     }
 
-    const toggleGeneDetails = async (name) => {
-      setSelectedGene(selectedGene !== name ? name : null);
-      setGeneDescription(null);
-
-      if (selectedGene === name) {
-        return;
-      }
-
-      setGeneDescription(
-        <>
-          <Link
-            href={`https://www.ncbi.nlm.nih.gov/gene?term=(${name}%5BGene%20Name%5D)%20AND%209606%5BTaxonomy%20ID%5D`}
-            className={classes.linkout}
-            {...linkoutProps}
-          >
-            NCBI Gene
-          </Link>
-          <Link
-            href={`https://www.ensembl.org/Homo_sapiens/Gene/Summary?g=${name}`}
-            className={classes.linkout}
-            style={{marginLeft: '2em', marginRight: '2em'}}
-            {...linkoutProps}
-          >
-            Ensembl
-          </Link>
-          <Link
-            href={`http://genemania.org/search/human/${name}`}
-            className={classes.linkout}
-            {...linkoutProps}
-          >
-            GeneMANIA
-          </Link>
-        </>
-      );
-    };
-
-    const isSelected = selectedGene === name;
-
     return (
       <ListItem key={idx} alignItems="flex-start">
         <ListItemText
           primary={
             <Grid container direction="row" justifyContent="space-between" alignItems='center'>
-              <Grid item onClick={() => toggleGeneDetails(name)}>
+              <Grid item>
                 <Grid container direction="row" justifyContent="flex-start" alignItems='center'>
-                  { isSelected ?
-                    <KeyboardArrowDownIcon fontSize="small" className={classes.listItemtemIcon} />
-                  :
-                    <KeyboardArrowRightIcon fontSize="small" className={classes.listItemtemIcon} />
-                  }
-                  <Link component="button" variant="body2" color="textPrimary" className={classes.geneName}>
+                  <DoubleArrowIcon className={classes.listItemtemIcon} />
+                  <Link
+                    href={`https://www.ncbi.nlm.nih.gov/gene?term=(${name}%5BGene%20Name%5D)%20AND%209606%5BTaxonomy%20ID%5D`}
+                    variant="body2"
+                    color="textPrimary"
+                    className={classes.geneName}
+                    {...linkoutProps}
+                  >
                     {name}
                   </Link>
                 </Grid>
@@ -273,11 +259,6 @@ export function GeneListPanel({ controller, searchResult }) {
               </Grid>
             </Grid>
           }
-          secondary={isSelected && (
-            <Typography variant="body2" color="textSecondary" className={classes.geneDesc}>
-              { geneDescription ? geneDescription : 'Loading...' }
-            </Typography>
-          )}
         />
       </ListItem>
     );
@@ -305,6 +286,13 @@ export function GeneListPanel({ controller, searchResult }) {
         />
       </ListItem>
     );
+  };
+
+  const handleSort = (evt, value) => {
+    if (value != null) {
+      setSort(value);
+      setGenes(sortGenes(genes, value));
+    }
   };
 
   const totalGenes = genes.length;
@@ -339,14 +327,40 @@ export function GeneListPanel({ controller, searchResult }) {
         dense
         subheader={
           <ListSubheader component="div">
-            <Typography display="block" variant="subtitle2" color="textPrimary" className={classes.title} gutterBottom>
-              Genes&nbsp;
-              <Typography display="inline" variant="body2" color="textSecondary">
-                ({!searchResult && totalGenes === 0 ? <em>loading...</em> : totalGenes})
-              </Typography>
-              :
-            </Typography>
-            <Divider />
+            <Grid container direction="row" justifyContent="space-between" alignItems='center'>
+              <Grid item>
+                <Typography display="block" variant="subtitle2" color="textPrimary" className={classes.title}>
+                  Genes&nbsp;
+                  <Typography display="inline" variant="body2" color="textSecondary">
+                    ({!searchResult && totalGenes === 0 ? <em>loading...</em> : totalGenes})
+                  </Typography>
+                  :
+                </Typography>
+              </Grid>
+              <Grid item>
+                <ToggleButtonGroup
+                  value={sort}
+                  exclusive
+                  onChange={handleSort}
+                >
+                  <ToggleButton value="alpha">
+                    <Tooltip arrow placement="top" title="Sort by gene NAME">
+                      <SortByAlphaIcon />
+                    </Tooltip>
+                  </ToggleButton>
+                  <ToggleButton value="up">
+                    <Tooltip arrow placement="top" title="Sort by RANK (from lowest to highest)">
+                      <KeyboardArrowUpIcon />
+                    </Tooltip>
+                  </ToggleButton>
+                  <ToggleButton value="down">
+                    <Tooltip arrow placement="top" title="Sort by RANK (from highest to lowest)">
+                      <KeyboardArrowDownIcon />
+                    </Tooltip>
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              </Grid>
+            </Grid>
           </ListSubheader>
         }
       >
