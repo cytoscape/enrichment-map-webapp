d1f482a3d43d10b0c9c9a41dec91f1707e3623d5
Christian Lopes <chrtannus@gmail.com>
2022-10-18 09:47:28 -0300

    Converts the main.js react components from class to functional+hooks.

--- src/client/components/network-editor/main.js
+++ src/client/components/network-editor/main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import _ from 'lodash';
@@ -9,7 +9,7 @@ import { NetworkEditorController } from './controller';
 import GeneListPanel from './gene-list-panel';
 import StyleLegend from './legend';
 
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 import { Drawer, Paper, List, Typography } from '@material-ui/core';
 import MuiAccordion from '@material-ui/core/Accordion';
@@ -22,242 +22,7 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 const LEGEND_HEADER_HEIGHT = 48;
 const LEGEND_CONTENT_HEIGHT = 160;
 
-export class Main extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchValue: "",
-      searchResult: null,
-      networkLoaded: false,
-      geneListIndexed: false,
-    };
-
-    this.controller = this.props.controller;
-    this.cy = this.controller.cy;
-    this.cyEmitter = new EventEmitterProxy(this.cy);
-
-    this._onNetworkLoaded = this._onNetworkLoaded.bind(this);
-    this._onGeneListIndexed = this._onGeneListIndexed.bind(this);
-  }
-
-  componentDidMount() {
-    this.controller.bus.on('networkLoaded', this._onNetworkLoaded);
-    this.controller.bus.on('geneListIndexed', this._onGeneListIndexed);
-
-    const container = document.getElementById('cy');
-    this.cy.mount(container);
-    this.cy.resize();
-
-    // function randomArg(...args) {
-    //   return args[Math.floor(Math.random() * args.length)];
-    // }
-    // this.eh = this.controller.eh = this.cy.edgehandles({
-    //   snap: true,
-    //   edgeParams: () => ({
-    //     // TODO temporary data
-    //     data: {
-    //       attr1: Math.random(), // betwen 0 and 1
-    //       attr2: Math.random() * 2.0 - 1.0, // between -1 and 1
-    //       attr3: randomArg("A", "B", "C")
-    //     }
-    //   })
-    // });
-
-    this.updateSelectionClass = _.debounce(() => {
-      const allEles = this.cy.elements();
-      const selectedEles = allEles.filter(':selected');
-      const unselectedEles = allEles.subtract(selectedEles);
-
-      this.cy.batch(() => {
-        if (allEles.length === unselectedEles.length) {
-          allEles.removeClass('unselected');
-        } else {
-          selectedEles.removeClass('unselected');
-          unselectedEles.addClass('unselected');
-        }
-      });
-    }, 64);
-
-    this.cyEmitter.on('select', () => {
-      this.updateSelectionClass();
-    }).on('unselect', () => {
-      this.updateSelectionClass();
-    }).on('remove', () => {
-      this.updateSelectionClass();
-    });
-  }
-
-  componentWillUnmount() {
-    this.controller.bus.removeListener('networkLoaded', this._onNetworkLoaded);
-    this.controller.bus.removeListener('geneListIndexed', this._onGeneListIndexed);
-    this.cyEmitter.removeAllListeners();
-  }
-
-  render() {
-    const { controller } = this;
-    const { classes, showControlPanel, drawerVariant, onContentClick } = this.props;
-    const { searchValue, searchResult, networkLoaded, geneListIndexed } = this.state;
-    
-    const cancelSearch = () => {
-      this.setState({ searchValue: "", searchResult: null });
-    };
-    const search = val => {
-      const query = val.trim();
-
-      if (val.length > 0) {
-        const res = controller.searchGenes(query);
-        this.setState({ searchValue: val, searchResult: res });
-      } else {
-        cancelSearch();
-      }
-    };
-
-    const Legend = withStyles({
-      root: {
-        boxShadow: 'none',
-        '&:before': {
-          display: 'none',
-        },
-        '&$expanded': {
-          margin: 0,
-          padding: 0,
-        },
-      },
-      expanded: {},
-    })(MuiAccordion);
-
-    const LegendSummary = withStyles({
-      root: {
-        minHeight: LEGEND_HEADER_HEIGHT,
-        '&$expanded': {
-          minHeight: LEGEND_HEADER_HEIGHT,
-        },
-      },
-      content: {
-        '&$expanded': {
-          margin: 0,
-          padding: 0,
-        },
-      },
-      expanded: {},
-    })(MuiAccordionSummary);
-
-    const LegendDetails = withStyles({
-      root: {
-        padding: 0,
-        margin: 0,
-      },
-    })(MuiAccordionDetails);
-
-    const LeftDrawer = () => {
-      return (
-        <Drawer
-          className={classes.drawer}
-          variant={drawerVariant}
-          anchor="left"
-          open={showControlPanel}
-          PaperProps={{
-            style: {
-              overflow: "hidden"
-            }
-          }}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.drawerContent}>
-            <header className={classes.drawerHeader}>
-              <Paper component="form" className={classes.root}>
-                <SearchBar
-                  autoFocus
-                  disabled={!networkLoaded || !geneListIndexed}
-                  className={classes.input}
-                  value={searchValue}
-                  onChange={val => search(val)}
-                  onCancelSearch={() => cancelSearch()}
-                />
-              </Paper>
-            </header>
-            <section className={classes.drawerSection}>
-              <List className={classes.geneList}>
-                {networkLoaded && geneListIndexed && (
-                  <GeneListPanel controller={controller} searchResult={searchResult} />
-                )}
-              </List>
-            </section>
-            <footer className={classes.drawerFooter}>
-              {networkLoaded && (
-                <Legend defaultExpanded>
-                  <LegendSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>Legend</Typography>
-                  </LegendSummary>
-                  <LegendDetails>
-                    <StyleLegend controller={controller} width={CONTROL_PANEL_WIDTH - 1} height={LEGEND_CONTENT_HEIGHT} />
-                  </LegendDetails>
-                </Legend>
-              )}  
-            </footer>
-          </div>
-        </Drawer>
-      );
-    };
-
-    const shiftCy = showControlPanel && drawerVariant === 'persistent';
-
-    return (
-      <div
-        className="network-editor-content"
-        onClick={onContentClick}
-      >
-        <LeftDrawer />
-        <div className={classes.cy}>
-          <div className={clsx(classes.cy, { [classes.cyShift]: shiftCy })}>
-            <div id="cy" className={classes.cy} />
-            <NetworkBackground controller={controller} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  _onNetworkLoaded() {
-    this.setState({ networkLoaded: true });
-  }
-
-  _onGeneListIndexed() {
-    this.setState({ geneListIndexed: true });
-  }
-}
-
-class NetworkBackground extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bgColor: 'white',
-    };
-    this.busProxy = new EventEmitterProxy(this.props.controller.bus);
-  }
-
-  componentDidMount() {
-    this.busProxy.on('setNetworkBackgroundColor', (color) => this.setState({ bgColor: color }));
-  }
-
-  componentWillUnmount() {
-    this.busProxy.removeAllListeners();
-  }
-
-  render() {
-    const { bgColor } = this.state;
-
-    return (
-      <div id="cy-background" style={{ backgroundColor: bgColor }} />
-    );
-  }
-}
-
-const useStyles = theme => ({
+const useStyles = makeStyles((theme) => ({
   drawer: {
     background: theme.palette.background.default,
     width: CONTROL_PANEL_WIDTH,
@@ -311,17 +76,232 @@ const useStyles = theme => ({
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
-});
+}));
+
+const NetworkBackground = ({ controller }) => {
+  const [bgColor, setBgColor] = useState('white');
+  
+  const busProxy = new EventEmitterProxy(controller.bus);
+
+  useEffect(() => {
+    busProxy.on('setNetworkBackgroundColor', (color) => setBgColor(color));
+
+    return function cleanup() {
+      busProxy.removeAllListeners();
+    };
+  }, []);
+  
+  return <div id="cy-background" style={{ backgroundColor: bgColor }} />;
+};
+
+const Main = ({ controller, showControlPanel, drawerVariant, onContentClick }) => {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResult, setSearchResult] = useState(null);
+  const [networkLoaded, setNetworkLoaded] = useState(false);
+  const [geneListIndexed, setGeneListIndexed] = useState(false);
+
+  const cy = controller.cy;
+  const cyEmitter = new EventEmitterProxy(cy);
+
+  const classes = useStyles();
+
+  const cancelSearch = () => {
+    setSearchValue('');
+    setSearchResult(null);
+  };
+  const search = (val) => {
+    const query = val.trim();
+
+    if (val.length > 0) {
+      const res = controller.searchGenes(query);
+      setSearchValue(val);
+      setSearchResult(res);
+    } else {
+      cancelSearch();
+    }
+  };
+
+  const onNetworkLoaded = () => {
+    setNetworkLoaded(true);
+  };
+  const onGeneListIndexed = () => {
+    setGeneListIndexed(true);
+  };
+
+  useEffect(() => {
+    controller.bus.on('networkLoaded', onNetworkLoaded);
+    controller.bus.on('geneListIndexed', onGeneListIndexed);
+
+    const container = document.getElementById('cy');
+    cy.mount(container);
+    cy.resize();
+
+    // function randomArg(...args) {
+    //   return args[Math.floor(Math.random() * args.length)];
+    // }
+    // this.eh = controller.eh = cy.edgehandles({
+    //   snap: true,
+    //   edgeParams: () => ({
+    //     // TODO temporary data
+    //     data: {
+    //       attr1: Math.random(), // betwen 0 and 1
+    //       attr2: Math.random() * 2.0 - 1.0, // between -1 and 1
+    //       attr3: randomArg("A", "B", "C")
+    //     }
+    //   })
+    // });
+
+    const updateSelectionClass = _.debounce(() => {
+      const allEles = cy.elements();
+      const selectedEles = allEles.filter(':selected');
+      const unselectedEles = allEles.subtract(selectedEles);
+
+      cy.batch(() => {
+        if (allEles.length === unselectedEles.length) {
+          allEles.removeClass('unselected');
+        } else {
+          selectedEles.removeClass('unselected');
+          unselectedEles.addClass('unselected');
+        }
+      });
+    }, 64);
+
+    const clearSearch = _.debounce(() => {
+      cancelSearch();
+    }, 128);
+
+    cyEmitter.on('select', () => {
+      updateSelectionClass();
+      clearSearch();
+    }).on('unselect', () => {
+      updateSelectionClass();
+    }).on('remove', () => {
+      updateSelectionClass();
+    });
+
+    return function cleanup() {
+      controller.bus.removeListener('networkLoaded', onNetworkLoaded);
+      controller.bus.removeListener('geneListIndexed', onGeneListIndexed);
+      cyEmitter.removeAllListeners();
+    };
+  }, []);
+
+  const Legend = withStyles({
+    root: {
+      boxShadow: 'none',
+      '&:before': {
+        display: 'none',
+      },
+      '&$expanded': {
+        margin: 0,
+        padding: 0,
+      },
+    },
+    expanded: {},
+  })(MuiAccordion);
+
+  const LegendSummary = withStyles({
+    root: {
+      minHeight: LEGEND_HEADER_HEIGHT,
+      '&$expanded': {
+        minHeight: LEGEND_HEADER_HEIGHT,
+      },
+    },
+    content: {
+      '&$expanded': {
+        margin: 0,
+        padding: 0,
+      },
+    },
+    expanded: {},
+  })(MuiAccordionSummary);
+
+  const LegendDetails = withStyles({
+    root: {
+      padding: 0,
+      margin: 0,
+    },
+  })(MuiAccordionDetails);
+
+  const LeftDrawer = () => {
+    return (
+      <Drawer
+        className={classes.drawer}
+        variant={drawerVariant}
+        anchor="left"
+        open={showControlPanel}
+        PaperProps={{
+          style: {
+            overflow: "hidden"
+          }
+        }}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerContent}>
+          <header className={classes.drawerHeader}>
+            <Paper component="form" className={classes.root}>
+              <SearchBar
+                autoFocus
+                disabled={!networkLoaded || !geneListIndexed}
+                className={classes.input}
+                value={searchValue}
+                onChange={val => search(val)}
+                onCancelSearch={() => cancelSearch()}
+              />
+            </Paper>
+          </header>
+          <section className={classes.drawerSection}>
+            <List className={classes.geneList}>
+              {networkLoaded && geneListIndexed && (
+                <GeneListPanel controller={controller} searchResult={searchResult} />
+              )}
+            </List>
+          </section>
+          <footer className={classes.drawerFooter}>
+            {networkLoaded && (
+              <Legend defaultExpanded>
+                <LegendSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography>Legend</Typography>
+                </LegendSummary>
+                <LegendDetails>
+                  <StyleLegend controller={controller} width={CONTROL_PANEL_WIDTH - 1} height={LEGEND_CONTENT_HEIGHT} />
+                </LegendDetails>
+              </Legend>
+            )}  
+          </footer>
+        </div>
+      </Drawer>
+    );
+  };
+    
+  const shiftCy = showControlPanel && drawerVariant === 'persistent';
+
+  return (
+    <div
+      className="network-editor-content"
+      onClick={onContentClick}
+    >
+      <LeftDrawer />
+      <div className={classes.cy}>
+        <div className={clsx(classes.cy, { [classes.cyShift]: shiftCy })}>
+          <div id="cy" className={classes.cy} />
+          <NetworkBackground controller={controller} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 NetworkBackground.propTypes = {
   controller: PropTypes.instanceOf(NetworkEditorController).isRequired,
 };
 Main.propTypes = {
-  classes: PropTypes.object.isRequired,
   controller: PropTypes.instanceOf(NetworkEditorController),
   showControlPanel: PropTypes.bool.isRequired,
   drawerVariant: PropTypes.string.isRequired,
   onContentClick: PropTypes.func.isRequired,
 };
 
-export default withStyles(useStyles)(Main);
+export default Main;
