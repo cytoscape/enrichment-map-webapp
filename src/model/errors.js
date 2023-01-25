2467b34de1b25b5c59d47ffc7764422aa1096e42
Max Franz <maxkfranz@gmail.com>
2023-01-25 08:58:25 -0500

    Add example usage of error on the client
    Ref: Show a warning/error message if EM service returns empty network. #46

--- src/model/errors.js
+++ src/model/errors.js
@@ -1,26 +1,105 @@
-/**
- * A `DocumentNotFoundError` is thrown when a `JsonSyncher` is not stored in the database and
- * a read operation fails.
- */
-export class DocumentNotFoundError extends Error {
-  constructor(dbName, docId, message){
-    super(`The document in database '${dbName}' with ID '${docId}' could not be found` + (message ? `: ${message}` : ''));
+export class QueryError extends Error {
+  static name = 'QueryError';
 
-    this.dbName = dbName;
-    this.docId = docId;
+  constructor(message) {
+    super(message ?? 'A query error occurred.'); // TODO: better default message
+
+    this.name = QueryError.name;
+  }
+
+  static fromJSON(json) {
+    const { name, message } = json;
+
+    switch (name) {
+      case QueryError.name:
+        return new QueryError(message);
+      case NumberOfColumnsError.name:
+        return NumberOfColumnsError.fromJSON(json);
+      case GeneLookupError.name:
+        return GeneLookupError.fromJSON(json);
+      case NonNumericValueError.name:
+        return NonNumericValueError.fromJSON(json);
+      // TODO: add new validation error types here
+    }
+  }
+
+  json() {
+    return {
+      name: this.name,
+      message: this.message
+    };
   }
 }
 
-/**
- * A `LoadConflictError` is thrown when a `JsonSyncher` has conflicting revisions on load.
- */
-export class LoadConflictError extends Error {
-  constructor(dbName, docId, rev, conflictingRevs){
-    super(`The document in database '${dbName}' with ID '${docId}' and revision '${rev}' has conflicting revisions '[${conflictingRevs.join(', ')}]'`);
+export class GeneLookupError extends QueryError {
+  static name = 'GeneLookupError';
 
-    this.dbName = dbName;
-    this.docId = docId;
-    this.rev = rev;
-    this.conflictingRevs = conflictingRevs;
+  constructor(genes, message) {
+    super(message ?? 'Several genes are invalid.'); // TODO: better default message
+
+    this.name = GeneLookupError.name;
+    this.genes = genes; // in format { row, column, name }
+  }
+
+  static fromJSON(json) {
+    return new GeneLookupError(json.genes, json.message);
+  }
+
+  json() {
+    return {
+      name: this.name,
+      message: this.message,
+      genes: this.genes
+    };
+  }
+}
+
+export class NumberOfColumnsError extends QueryError {
+  static name = 'NumberOfColumnsError';
+ 
+  constructor(columns, message) {
+    super(message ?? 'The number of columns is wrong!'); // TODO: better default message
+
+    this.name = NumberOfColumnsError.name;
+    this.columns = columns;
+  }
+
+  static fromJSON(json) {
+    return new NumberOfColumnsError(json.columns, json.message);
+  }
+
+  json() {
+    return {
+      name: this.name,
+      message: this.message,
+      columns: this.columns
+    };
+  }
+}
+
+export class NonNumericValueError extends QueryError {
+  static name = 'NonNumericValueError';
+
+  constructor(value, column, row, message) {
+    super(message ?? `Non-numeric value in input file at ${column}:${row} with value '${value}'.`);
+
+    this.name = NonNumericValueError.name;
+    this.value = value;
+    this.column = column;
+    this.row = row;
+  }
+
+  static fromJSON(json) {
+    return new NonNumericValueError(json.value, json.column, json.row, json.message);
+  }
+
+  json() {
+    return {
+      name: this.name,
+      message: this.message,
+      value: this.value,
+      column: this.column,
+      row: this.row
+    };
   }
 }
