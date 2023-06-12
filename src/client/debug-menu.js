ab57ee26b2d793e5d45a99b5528e202cd4a681da
Mike Kucera <mikekucera@gmail.com>
2023-06-12 13:59:46 -0400

    convert DebugMenu to hooks

--- src/client/debug-menu.js
+++ src/client/debug-menu.js
@@ -1,71 +1,34 @@
-import React, { Component } from 'react';
-import { Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
-import { Button } from '@material-ui/core';
+import React, { useReducer, useEffect } from 'react';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@material-ui/core';
 import Mousetrap from 'mousetrap';
 import PropTypes from 'prop-types';
-import { NODE_ENV } from './env';
 
-export class DebugMenu extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      open: false
-    };
-  }
+export function DebugMenu({ children }) {
 
-  componentDidMount() {
-    Mousetrap.bind('`', this.keyHandler = () => {
-      this.toggle();
-    });
-  }
+  const [ open, toggle ] = useReducer(x => !x, false);
 
-  componentWillUnmount() {
-    Mousetrap.unbind('`', this.keyHandler);
-  }
+  useEffect(() => {
+    Mousetrap.bind('`', toggle);
+    return () => Mousetrap.unbind('`', toggle);
+  }, []); 
 
-  open() {
-    this.setState({ open: true });
-  }
-
-  close() {
-    this.setState({ open: false });
-  }
-
-  toggle() {
-    if (this.state.open) {
-      this.close();
-    } else {
-      this.open();
-    }
-  }
-
-  render() {
-    const { open } = this.state;
-    const { children } = this.props;
-
-    // TODO: completely disable rendering of the debug menu in production releases?
-    // if (NODE_ENV !== 'development') {
-    //   return <div></div>;
-    // }
-
-    return (
-      <Dialog
-        open={open}
-        onClose={() => {}}
-      >
-        <DialogTitle>Debug Menu</DialogTitle>
-        <DialogContent>
-          {children}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => this.close()} color="primary">
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
+  return (
+    <Dialog
+      open={open}
+      onClose={() => {}}
+    >
+      <DialogTitle>Debug Menu</DialogTitle>
+      <DialogContent>
+        {children}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => this.close()} color="primary">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
 DebugMenu.propTypes = {
