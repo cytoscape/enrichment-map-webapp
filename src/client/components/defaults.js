5f51aa155fd44d97e3990b000874854ab2ef518a
Christian Lopes <chrtannus@gmail.com>
2023-09-11 15:48:00 -0300

    Decreases the default network padding.

--- src/client/components/defaults.js
+++ src/client/components/defaults.js
@@ -1,4 +1,4 @@
-export const DEFAULT_PADDING = 50;
+export const DEFAULT_PADDING = 10;
 export const CONTROL_PANEL_WIDTH = 320;
 
 export const linkoutProps = { target: "_blank",  rel: "noreferrer", underline: "hover" };
