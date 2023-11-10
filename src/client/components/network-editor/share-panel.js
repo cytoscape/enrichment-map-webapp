d22d4fb78115d5c8e617fd3aa334264646c3b046
Mike Kucera <mikekucera@gmail.com>
2023-11-10 16:49:29 -0500

    Start of new network image export that includes bubbles (still PNG).

--- src/client/components/network-editor/share-panel.js
+++ src/client/components/network-editor/share-panel.js
@@ -8,32 +8,58 @@ import { getSVGString } from './legend-svg';
 import { NODE_COLOR_SVG_ID } from './legend-button';
 import InsertDriveFileOutlinedIcon from '@material-ui/icons/InsertDriveFileOutlined';
 import LinkIcon from '@material-ui/icons/Link';
+import { Canvg, presets } from 'canvg';
 
 
 const ImageSize = {
-  SMALL:  { value:'SMALL',  scale: 0.3 },
-  MEDIUM: { value:'MEDIUM', scale: 1.0 },
-  LARGE:  { value:'LARGE',  scale: 3.0 },
-};
-
-const ImageArea = {
-  FULL: 'full',
-  VIEW: 'view',
+  SMALL:  { value:'SMALL',  scale: 0.3, svgTransform: 'translate(12,12)scale(0.6)' },
+  MEDIUM: { value:'MEDIUM', scale: 1.0, svgTransform: 'translate(55,28)scale(2)' },
+  LARGE:  { value:'LARGE',  scale: 2.0, svgTransform: 'translate(110,56)scale(6)' },
 };
 
 
-function wait(millis, value="") {
-  return new Promise(resolve => setTimeout(resolve, millis, value));
-}
+// async function createNetworkImageBlob(controller, imageSize, imageArea=ImageArea.FULL) {
+//   return await controller.cy.png({
+//     output: 'blob-promise',
+//     bg: 'white',
+//     full: imageArea === ImageArea.FULL,
+//     scale: imageSize.scale,
+//   });
+// }
 
-async function createNetworkImageBlob(controller, imageSize, imageArea=ImageArea.FULL) {
-  return await controller.cy.png({
-    output: 'blob-promise',
+async function createNetworkImageBlob(controller, imageSize) {
+  const cyoptions = {
+    output: 'blob',
     bg: 'white',
-    full: imageArea === ImageArea.FULL,
+    full: true, 
     scale: imageSize.scale,
-  });
+  };
+
+  // render the network
+  const renderer = controller.cy.renderer();
+  const cyCanvas = renderer.bufferCanvasImage(cyoptions);
+  const { width, height } = cyCanvas;
+
+  // render the bubbleSet svg layer
+  const svgElem = controller.bubbleSets.layer.node.parentNode.cloneNode(true);
+  // Setting the 'transform' attribute to a hardcoded value is a hack. There must be some way to compute it.
+  svgElem.firstChild.setAttribute('transform', imageSize.svgTransform); // firstChild is a <g> tag
+
+  const svgCanvas = new OffscreenCanvas(width, height);
+  const ctx = svgCanvas.getContext('2d');
+  let v = await Canvg.from(ctx, svgElem.innerHTML, presets.offscreen());
+  await v.render();
+
+  // combine the layers
+  const combinedCanvas = new OffscreenCanvas(width, height);
+  const combinedCtx = combinedCanvas.getContext('2d');
+  combinedCtx.drawImage(cyCanvas, 0, 0);
+  combinedCtx.drawImage(svgCanvas, 0, 0);
+
+  const blob = await combinedCanvas.convertToBlob();
+  return blob;
 }
+
 
 async function createSVGLegendBlob(svgID) {
   const svg = getSVGString(svgID);
@@ -60,20 +86,24 @@ async function saveZip(controller, zip, type) {
 
 
 async function handleExportImageArchive(controller) {
-  const blobs = await Promise.all([
-    createNetworkImageBlob(controller, ImageSize.SMALL),
-    createNetworkImageBlob(controller, ImageSize.MEDIUM),
-    createNetworkImageBlob(controller, ImageSize.LARGE),
-    // createSVGLegendBlob(NODE_COLOR_SVG_ID), // TODO fix the SVG legend
-  ]);
+  // const blobs = await Promise.all([
+  //   createNetworkImageBlob(controller, ImageSize.SMALL),
+  //   createNetworkImageBlob(controller, ImageSize.MEDIUM),
+  //   createNetworkImageBlob(controller, ImageSize.LARGE),
+  //   // createSVGLegendBlob(NODE_COLOR_SVG_ID), // TODO fix the SVG legend
+  // ]);
 
-  const zip = new JSZip();
-  zip.file('enrichment_map_small.png',  blobs[0]);
-  zip.file('enrichment_map_medium.png', blobs[1]);
-  zip.file('enrichment_map_large.png',  blobs[2]);
-  // zip.file('node_color_legend.svg',     blobs[3]); // TODO
+  // const zip = new JSZip();
+  // zip.file('enrichment_map_small.png',  blobs[0]);
+  // zip.file('enrichment_map_medium.png', blobs[1]);
+  // zip.file('enrichment_map_large.png',  blobs[2]);
+  // // zip.file('node_color_legend.svg',     blobs[3]); // TODO
 
-  saveZip(controller, zip, 'images');
+  // saveZip(controller, zip, 'images');
+
+  const size = ImageSize.MEDIUM;
+  const blob = await createNetworkImageBlob(controller, size);
+  saveAs(blob, `network_image_${size.value}.png`);
 }
 
 
@@ -127,26 +157,31 @@ export function ShareMenu({ controller, target, visible, onClose = ()=>null, set
     snack.showMessage("Link copied to clipboard");
   };
 
+  const spinnerUntilDone = async (promise, message) => {
+    const delay = (millis) => new Promise(r => setTimeout(r, millis, 'delay'));
+
+    // Only show spinner if it takes longer than the inital delay
+    const value = await Promise.race([ promise, delay(500) ]);
+    if(value === 'delay') // if the delay promise finished first
+      snack.showSpinner(message);
+
+    await promise; // wait for the export to finish if it hasn't already
+    snack.close();
+  };
+
   const handleExportImages = async () => {
     onClose();
     setImageExportEnabled(false);
-    await handleExportImageArchive(controller);
+    const promise = handleExportImageArchive(controller);
+    await spinnerUntilDone(promise, "Exporting network images...");
     setImageExportEnabled(true); 
   };
 
   const handleExportData = async () => {
     onClose();
     setDataExportEnabled(false);
-
-    const dataExportPromise = handleExportDataArchive(controller);
-
-    const value = await Promise.race([ dataExportPromise, wait(1000,"waiting") ]);
-
-    if(value === "waiting") // if the "waiting" promise resolved first then show a progress indicator
-      snack.showSpinner("Exporting enrichment data...");
-
-    await dataExportPromise; // wait for the export to finish
-    snack.close();
+    const promise = handleExportDataArchive(controller);
+    await spinnerUntilDone(promise, "Exporting enrichment data...");
     setDataExportEnabled(true);
   };
 
