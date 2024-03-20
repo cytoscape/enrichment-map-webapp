d1894453da03c9f923ff40ddd6adf0e97c9416f0
Christian Lopes <chrtannus@gmail.com>
2024-03-20 16:50:59 -0300

    Revised instructions for file upload (bigger RNS-Seq table, arrows, renamed columns, etc.) -- #269

--- src/client/components/home/upload-panel.js
+++ src/client/components/home/upload-panel.js
@@ -2,42 +2,94 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
+import theme from '../../theme';
 import { RNA_SEQ, PRE_RANKED } from './upload-controller';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import { Grid, Paper, Typography, Tooltip, Link } from '@material-ui/core';
+import { ArcherContainer, ArcherElement } from 'react-archer';
+import { Paper, Typography, Link } from '@material-ui/core';
 import { Accordion, AccordionDetails, AccordionSummary, Radio } from '@material-ui/core';
 import { Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const useStyles = makeStyles((theme) => ({
-  description: {
-    marginBottom: theme.spacing(2.5),
-    [theme.breakpoints.down('xs')]: {
-      marginBottom: theme.spacing(1),
-      fontSize: '0.85rem',
-    },
+
+const createRankedRow = (gene, rank) => {
+  return { gene, rank };
+};
+
+const createRnaSeqRow = (gene, sample1, sample2, others, samplen) => {
+  return { gene, sample1, sample2, others, samplen };
+};
+
+const RANKED_HEADER = [
+  {
+    id: 'gene',
+    label: 'Gene',
   },
-  details: {
-    marginTop: 0,
-    [theme.breakpoints.down('xs')]: {
-      marginBlockStart: 0,
-      marginBlockEnd: theme.spacing(1),
-      fontSize: '0.85rem',
-    },
+  {
+    id: 'rank',
+    label: 'Rank',
   },
+];
+const RANKED_ROWS = [
+  createRankedRow('ANKRD9',  50.62464011),
+  createRankedRow('LYL1',    21.57521227),
+  createRankedRow('ABHD17A', 11.96322182),
+  createRankedRow('EVA1B',   -10.07102147),
+  createRankedRow('FOXO3',   -24.77997781),
+];
+
+const RNASEQ_HEADER = [
+  {
+    id: 'gene',
+    label: 'Gene',
+  },
+  {
+    id: 'sample1',
+    label: 'Sample 1',
+  },
+  {
+    id: 'sample2',
+    label: 'Sample 2',
+  },
+  {
+    id: 'others',
+    label: '...',
+  },
+  {
+    id: 'samplen',
+    label: <>Sample <i>n</i></>,
+  }
+];
+const RNASEQ_ROWS = [
+  createRnaSeqRow('MUC1',   950, 550, '...', 471),
+  createRnaSeqRow('MUC6',   103, 338, '...', 603),
+  createRnaSeqRow('GCNT1',  890, 640, '...', 209),
+  createRnaSeqRow('B3GNT9', 700, 910, '...', 929),
+  createRnaSeqRow('GALNT9', 730, 804, '...', 996),
+];
+
+//==[ SampleTable ]===================================================================================================
+
+const useSampleTableStyles = makeStyles((theme) => ({
   tableContainer: {
-    maxWidth: 240,
     background: theme.palette.background.default,
+    marginLeft: 20,
+    marginRight: 20,
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+      marginRight: 0,
+    },
   },
   tableHead: {
-    textTransform: 'capitalize',
   },
   tableCell: {
     fontSize: '0.85em',
     fontFamily: 'Monaco,Courier New,Monospace',
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
     color: theme.palette.text.disabled,
     [theme.breakpoints.down('xs')]: {
       paddingTop: theme.spacing(0.25),
@@ -48,120 +100,58 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(1.25),
     paddingBottom: theme.spacing(1.25),
     borderBottom: `3px double ${theme.palette.divider}`,
+    verticalAlign: 'top',
     [theme.breakpoints.down('xs')]: {
       paddingTop: 'inherit',
       paddingBottom: 'inherit',
     },
   },
-  accordionSummaryContent: {
-      alignItems: 'center',
+  geneTableHeadCell: {
+    borderTop: `2px double ${theme.palette.text.accent}`,
   },
-  accordionHead: {
-    fontSize: 'theme.typography.pxToRem(15)',
-    fontWeight: theme.typography.fontWeightRegular,
+  sample1TableHeadCell: {
+    borderTop: `2px double ${theme.palette.primary.main}`,
   },
-  accordionDetails: {
-    [theme.breakpoints.down('xs')]: {
-      paddingTop: 0,
-    },
+  sample2TableHeadCell: {
+    borderTop: `2px double ${theme.palette.primary.main}`,
   },
-  spotlightLink: {
-    color: theme.palette.text.secondary,
-    borderBottom: `1px dotted ${theme.palette.text.disabled}`,
-    cursor: 'default',
-    "&:hover": {
-      color: theme.palette.text.primary,
-      borderBottom: `1px dotted ${theme.palette.text.secondary}`,
-    }
+  othersTableHeadCell: {
+    borderTop: `2px double ${theme.palette.primary.main}`,
+  },
+  samplenTableHeadCell: {
+    borderTop: `2px double ${theme.palette.primary.main}`,
+  },
+  rankTableHeadCell: {
+    borderTop: `2px double ${theme.palette.primary.main}`,
   },
   spotlight: {
     backgroundColor: theme.palette.action.selected,
   },
-  demoThumbnail: {
-    borderRadius: '15px',
-    width: '300px'
-  },
-  pcLink: {
-    display: "flex", 
-    alignItems: "center"
-  }
 }));
 
-const createRankedRow = (gene, rank) => {
-  return { gene, rank };
-};
-
-const createRnaSeqRow = (gene, exp1, exp2) => {
-  return { gene, exp1, exp2 };
-};
-
-const RANKED_HEADER = [
-  {
-    id: 'gene',
-    tooltip: 'The gene ID (Ensembl or HGNC)',
-  },
-  {
-    id: 'rank',
-    tooltip: 'The gene rank value',
-  },
-];
-const RANKED_ROWS = [
-  createRankedRow('ANKRD9',  50.62464011),
-  createRankedRow('LYL1',    41.57521227),
-  createRankedRow('ABHD17A', 21.96322182),
-  createRankedRow('EVA1B',   20.35075839),
-  createRankedRow('FOXO3',   11.93840251),
-];
-
-const RNASEQ_HEADER = [
-  {
-    id: 'gene',
-    tooltip: 'The gene ID (Ensembl or HGNC)',
-  },
-  {
-    id: 'exp1',
-    tooltip: 'The EXPERIMENT expression value',
-  },
-  {
-    id: 'exp2',
-    tooltip: 'The CONTROL expression value',
-  }
-];
-const RNASEQ_ROWS = [
-  createRnaSeqRow('MUC1',   950, 550),
-  createRnaSeqRow('MUC6',   890, 640),
-  createRnaSeqRow('GCNT1',  670, 860),
-  createRnaSeqRow('B3GNT9', 700, 910),
-  createRnaSeqRow('GALNT9', 730, 800),
-  createRnaSeqRow('MUCL1',  800, 780),
-];
-
-//==[ SampleTable ]===================================================================================================
-
 const SampleTable = ({ tableHead, tableRows, spotlight, isMobile }) => {
-  const classes = useStyles();
+  const classes = useSampleTableStyles();
   const keys = Object.keys(tableRows[0]);
   const spotlightTokens = spotlight ? spotlight.split(',') : [];
 
   return (
     <Paper className={classes.tableContainer} variant="outlined">
       <Table size="small">
-        <TableHead className={clsx(classes.tableHead, { [classes.spotlight]: (spotlightTokens.includes('header')) })}>
+        <TableHead className={classes.tableHead}>
           <TableRow>
-          {tableHead.map(({ id, tooltip }, idx) => (
-            <TableCell
-              key={id}
-              align={idx === 0 ? 'left' : 'center'}
-              className={clsx(classes.tableCell, { [classes.tableHeadCell]: true, [classes.spotlight]: (spotlightTokens.includes(id)) })}
-            >
-              <Tooltip title={tooltip}>
-                <i>{id}</i>
-              </Tooltip>
-            </TableCell>
+          {tableHead.map(({ id, label }, idx) => (
+            <ArcherElement key={id} id={`${id}-th`}>
+              <TableCell
+                align={idx === 0 ? 'left' : 'center'}
+                className={clsx(classes.tableCell, { [classes.tableHeadCell]: true, [classes[`${id}TableHeadCell`]]: true, [classes.spotlight]: (spotlightTokens.includes(id)) })}
+              >
+                {label}
+              </TableCell>
+            </ArcherElement>
           ))}
           </TableRow>
         </TableHead>
-        <TableBody className={clsx({ [classes.spotlight]: (spotlightTokens.includes('data')) })}>
+        <TableBody>
         {tableRows.map((row, ridx) => (
           (!isMobile || ridx < 3) && (
           <TableRow key={row.gene}>
@@ -189,8 +179,22 @@ SampleTable.propTypes = {
 
 //==[ FormatAccordion ]===============================================================================================
 
-const FormatAccordion = ({ isMobile, id, title, summary, tableHead, tableRows, children, spotlight, selected, onChange }) => {
-  const classes = useStyles();
+const useFormatAccordionStyles = makeStyles((theme) => ({
+  accordionSummaryContent: {
+    alignItems: 'center',
+  },
+  accordionHead: {
+    fontSize: 'theme.typography.pxToRem(15)',
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  accordionDetails: {
+    width: '100%',
+    justifyContent: 'center',
+  },
+}));
+
+const FormatAccordion = ({ id, title, children, selected, onChange }) => {
+  const classes = useFormatAccordionStyles();
 
   return (
     <Accordion expanded={selected} variant="outlined" onChange={onChange(id)}>
@@ -204,27 +208,7 @@ const FormatAccordion = ({ isMobile, id, title, summary, tableHead, tableRows, c
         <Typography className={classes.accordionHead}>{ title }</Typography>
       </AccordionSummary>
       <AccordionDetails className={classes.accordionDetails}>
-        <Grid container direction="column" alignItems="flex-start">
-          <Grid item>
-            <Grid
-              container
-              direction={isMobile ? 'column' : 'row'}
-              alignItems={isMobile ? 'center' : 'flex-start'}
-              justifyContent="space-between"
-              spacing={isMobile ? 2 : 0}
-            >
-              <Grid item sm={6}>
-                <SampleTable tableHead={tableHead} tableRows={tableRows} spotlight={spotlight} isMobile={isMobile} />
-              </Grid>
-              <Grid item sm={6}>
-                <Typography component="div" variant="body2" color="textSecondary">
-                  { children }
-                  { summary() }
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+        { children }
       </AccordionDetails>
     </Accordion>
   );
@@ -232,51 +216,60 @@ const FormatAccordion = ({ isMobile, id, title, summary, tableHead, tableRows, c
 FormatAccordion.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  summary: PropTypes.func.isRequired,
-  isMobile: PropTypes.bool.isRequired,
-  tableHead: PropTypes.array.isRequired,
-  tableRows: PropTypes.array.isRequired,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired,
-  spotlight: PropTypes.string,
   selected: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
 };
 
-//==[ Spotlight ]=====================================================================================================
-
-const Spotlight = ({ children, target, onMouseOver, onMouseOut }) => {
-  const classes = useStyles();
-
-  return (
-    <span
-      className={classes.spotlightLink}
-      onMouseOver={() => onMouseOver(target)}
-      onMouseOut={onMouseOut}
-    >
-      { children }
-    </span>
-  );
-};
-Spotlight.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired,
-  target: PropTypes.string.isRequired,
-  onMouseOver: PropTypes.func.isRequired,
-  onMouseOut: PropTypes.func.isRequired,
-};
-
 //==[ UploadPanel ]===================================================================================================
+
+const useUploadPanelStyles = makeStyles((theme) => ({
+  description: {
+    marginBottom: theme.spacing(2.5),
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: theme.spacing(1),
+      fontSize: '0.85rem',
+    },
+  },
+  details: {
+    marginTop: 0,
+    [theme.breakpoints.down('xs')]: {
+      marginBlockStart: 0,
+      marginBlockEnd: theme.spacing(1),
+      fontSize: '0.85rem',
+    },
+  },
+  archerContainer: {
+    width: '100%',
+  },
+  legendContainer: {
+    width: '100%',
+  },
+  legend: {
+    position:'absolute',
+    padding: 5,
+    fontSize: '0.85em',
+    color: theme.palette.text.secondary,
+    cursor: 'default',
+    border: `1px solid transparent`,
+    "&:hover": {
+      color: theme.palette.text.primary,
+    },
+  },
+  pcLink: {
+    display: "flex", 
+    alignItems: "center"
+  }
+}));
 
 export function UploadPanel({ isMobile, initialFormat, onFormatChanged }) {
   const [ format, setFormat ] = useState(initialFormat);
   const [ spotlight, setSpotlight ] = useState();
 
-  const classes = useStyles();
+  const classes = useUploadPanelStyles();
   
   const handleChange = (value) => (event, select) => {
     if (select) { // clicking an expanded accordion should not collapse it
@@ -291,18 +284,6 @@ export function UploadPanel({ isMobile, initialFormat, onFormatChanged }) {
     setSpotlight(null);
   };
 
-  const GeneNameInfo = () =>
-    <>
-      <Spotlight target="gene" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Gene column</Spotlight>: gene <code>names</code>.
-    </>;
-
-  const summary = () =>
-    <>
-       Your spreadsheet must have a&nbsp;
-       <Spotlight target="header" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>header</Spotlight> row and&nbsp;
-       <Spotlight target="data" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>data</Spotlight> rows in that order, with whatever row titles you prefer.
-    </>;
-
   return (
     <>
       <Typography component="p" variant="body1" className={classes.description}>
@@ -311,37 +292,88 @@ export function UploadPanel({ isMobile, initialFormat, onFormatChanged }) {
       <FormatAccordion
         id={RNA_SEQ}
         title="RNA-Seq Expression Data"
-        summary={summary}
-        isMobile={isMobile}
-        tableHead={RNASEQ_HEADER}
-        tableRows={RNASEQ_ROWS}
-        spotlight={spotlight}
         selected={format === RNA_SEQ}
         onChange={handleChange}
       >
-        <p className={classes.details}>Your spreadsheet must have 3 or more columns, as follows:</p>
-        <ol className={classes.details}>
-          <li><GeneNameInfo /></li>
-          <li><Spotlight target="exp1,exp2" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}><code>Expression</code> columns</Spotlight>: numeric expression values.</li>
-          <li>Other columns: must be marked as &ldquo;ignored&rdquo; in the next step.</li>
-        </ol>
+        <ArcherContainer
+          strokeWidth={1}
+          strokeDasharray="1,4"
+          lineStyle="straight"
+          className={classes.archerContainer}
+        >
+          <div className={classes.legendContainer} style={{ height: 160 }}>
+            <ArcherElement
+              id="legend1"
+              relations={[{ targetId: 'gene-th', sourceAnchor: 'bottom', targetAnchor: 'top', style: { strokeColor: theme.palette.text.accent } }]}
+            >
+              <div
+                className={classes.legend}
+                style={{ top: 0, left: 10 }}
+                onMouseOver={() => handleMouseOver('gene')}
+                onMouseOut={() => handleMouseOut()}
+              >
+                <b>1<sup>st</sup> Column:</b> the gene IDs<br/>&#40;from Ensembl or HGNC&#41;
+              </div>
+            </ArcherElement>
+            <ArcherElement
+              id="legend2"
+              relations={[{ targetId: 'sample2-th', sourceAnchor: 'bottom', targetAnchor: 'top', style: { strokeColor: theme.palette.primary.main } }]}
+            >
+              <div
+                className={classes.legend}
+                style={{ top: 60, left: 100 }}
+                onMouseOver={() => handleMouseOver('sample1,sample2,others,samplen')}
+                onMouseOut={() => handleMouseOut()}
+              >
+                <b>Expression Columns:</b> at least 2<br/>&#40;numeric expression values&#41;
+              </div>
+            </ArcherElement>
+          </div>
+          <SampleTable tableHead={RNASEQ_HEADER} tableRows={RNASEQ_ROWS} spotlight={spotlight} isMobile={isMobile} />
+        </ArcherContainer>
       </FormatAccordion>
       <FormatAccordion
         id={PRE_RANKED}
         title="Pre-Ranked Gene List"
-        summary={summary}
-        isMobile={isMobile}
-        tableHead={RANKED_HEADER}
-        tableRows={RANKED_ROWS}
-        spotlight={spotlight}
         selected={format === PRE_RANKED}
         onChange={handleChange}
       >
-        <p className={classes.details}>Your spreadsheet must have exactly 2 columns, as follows:</p>
-        <ol className={classes.details} >
-          <li><GeneNameInfo /></li>
-          <li><Spotlight target="rank" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Second column</Spotlight>: the numeric <code>ranks</code>.</li>
-        </ol>
+        <ArcherContainer
+          strokeWidth={1}
+          strokeDasharray="1,4"
+          lineStyle="straight"
+          className={classes.archerContainer}
+        >
+          <div className={classes.legendContainer} style={{ height: 160 }}>
+            <ArcherElement
+              id="legend1"
+              relations={[{ targetId: 'gene-th', sourceAnchor: 'bottom', targetAnchor: 'top', style: { strokeColor: theme.palette.text.accent } }]}
+            >
+              <div
+                className={classes.legend}
+                style={{ top: 0, left: 10 }}
+                onMouseOver={() => handleMouseOver('gene')}
+                onMouseOut={() => handleMouseOut()}
+              >
+                <b>1<sup>st</sup> Column:</b> the gene IDs<br/>&#40;from Ensembl or HGNC&#41;
+              </div>
+            </ArcherElement>
+            <ArcherElement
+              id="legend2"
+              relations={[{ targetId: 'rank-th', sourceAnchor: 'bottom', targetAnchor: 'top', style: { strokeColor: theme.palette.primary.main } }]}
+            >
+              <div
+                className={classes.legend}
+                style={{ top: 60, left: 120 }}
+                onMouseOver={() => handleMouseOver('rank')}
+                onMouseOut={() => handleMouseOut()}
+              >
+                <b>2<sup>nd</sup> Column:</b> the numeric ranks
+              </div>
+            </ArcherElement>
+          </div>
+          <SampleTable tableHead={RANKED_HEADER} tableRows={RANKED_ROWS} spotlight={spotlight} isMobile={isMobile} />
+        </ArcherContainer>
       </FormatAccordion>
     </>
   );
@@ -352,14 +384,22 @@ UploadPanel.propTypes = {
   onFormatChanged: PropTypes.func.isRequired,
 };
 
+//==[ DemoPanel ]=====================================================================================================
 
-export function DemoPanel({ isMobile }) {
-  const classes = useStyles();
+const useDemoPanelStyles = makeStyles(() => ({
+  thumbnail: {
+    borderRadius: '15px',
+    width: '300px'
+  },
+}));
+
+export function DemoPanel() {
+  const classes = useDemoPanelStyles();
   return <>
     <Typography component="p" variant="body1" className={classes.description}>
       Create a demo network from real world RNA-Seq data.
     </Typography>
-    <img className={classes.demoThumbnail} src="/images/demo_small.png" alt="thumbnail of demo network" />
+    <img className={classes.thumbnail} src="/images/demo_small.png" alt="thumbnail of demo network" />
     <br/><br/>
     <Typography component="p" variant="body1">
       The data used to create this network is described in detail in the <br/>
@@ -373,6 +413,5 @@ export function DemoPanel({ isMobile }) {
   </>;
 }
 DemoPanel.propTypes = {
-  isMobile: PropTypes.bool,
 };
 
